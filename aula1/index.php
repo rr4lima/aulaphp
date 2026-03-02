a95bae4c8bc23f89911db40a5c2c2bf2ee0a0301
Rafa <165973120+rr4lima@users.noreply.github.com>
2026-03-02 17:48:42 -0300

    Add files via upload

---
+++ aula1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula PHP</title>
+</head>
+<body>
+    <h1>Aula 1 de PHP</h1>
+
+    <?php
+        $nome = "Kevin";
+        echo ("Olá, php, sou o " . $nome . "!");
+        echo $nome
+  ?>
+
+  <?=
+  $nome;
+  echo ("Willshon");
+  ?>
+    
+</body>
+</html>
