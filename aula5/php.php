e894b13953b7a5bb41b58779b281e23bb776c034
Rafa <165973120+rr4lima@users.noreply.github.com>
2026-03-13 17:26:52 -0300

    Add files via upload

--- aula5/php.php
+++ aula5/php.php
@@ -1,20 +1,50 @@
-
-
 <?php
 
 $arquivo = "usuarios.json";
+if(isset($_POST["nome"])){
+
+$nome = trim($_POST["nome"]);
+$idade = trim($_POST["idade"]);
+$curso = trim($_POST["curso"]);
+
+$novoUsuario = [
+    "nome" => $nome,
+    "idade" => $idade,
+    "curso" => $curso
+];
+
+if(file_exists($arquivo)){
+$conteudo = file_get_contents($arquivo);
+$usuarios = json_decode($conteudo, true);
+}else{
+$usuarios = [];
+}
+
+$usuarios[] = $novoUsuario;
+
+
+file_put_contents($arquivo, json_encode($usuarios, JSON_PRETTY_PRINT));
+
+echo "Aluno cadastrado com sucesso!";
+
+}
+
+
+if(file_exists($arquivo)){
 
 $conteudo = file_get_contents($arquivo);
 $usuarios = json_decode($conteudo, true);
 
-    echo "<pre>";
-    print_r($usuarios);
-    echo "</pre>";
-    
-$nome = $_POST["nome"];
-$idade = $_POST['idade'];
-$curso = $_POST['curso'];
+echo "<h2>Lista de alunos</h2>";
 
+foreach($usuarios as $u){
 
+echo "Nome: ".$u["nome"]."<br>";
+echo "Idade: ".$u["idade"]."<br>";
+echo "Curso: ".$u["curso"]."<br><br>";
+
+}
+
+}
 
 ?>
